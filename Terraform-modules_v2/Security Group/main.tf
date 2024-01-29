8adea2883057ca1d8ca6f437e69e56905f39deb1
Abhishek Dhondge <abhishekdhondge16.ad@gmail.com>
2024-01-29 11:22:45 +0530

    ..

--- Terraform-modules_v2/Security Group/main.tf
+++ Terraform-modules_v2/Security Group/main.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "sg" {
       to_port          = var.rules[ingress.value][1]
       protocol         = var.rules[ingress.value][2]
       description      = var.rules[ingress.value][3]
-      cidr_blocks      = length(var.rules[ingress.value]) > 4 ? var.rules[ingress.value][4] : []
-      security_groups  = length(var.rules[ingress.value]) > 5 ? var.rules[ingress.value][5] : []
+      cidr_blocks      = var.ingress_cidr_blocks
+      security_groups  = var.ingress_security_groups
     }
   }
 
@@ -22,8 +22,8 @@ resource "aws_security_group" "sg" {
       to_port          = var.rules[egress.value][1]
       protocol         = var.rules[egress.value][2]
       description      = var.rules[egress.value][3]
-      cidr_blocks      = length(var.rules[egress.value]) > 4 ? var.rules[egress.value][4] : []
-      security_groups  = length(var.rules[egress.value]) > 5 ? var.rules[egress.value][5] : []
+      cidr_blocks      = var.egress_cidr_blocks
+      security_groups  = var.egress_security_groups
     }
   }
   tags = {
--- Terraform-modules_v2/Security Group/variables.tf
+++ Terraform-modules_v2/Security Group/variables.tf
@@ -21,11 +21,28 @@ variable "ingress_rules" {
   type        = list(string)
 }
 
+variable "ingress_cidr_blocks" {
+  description = "A list of cidr blocks for ingress"
+  type = list(string)
+}
+
+variable "ingress_security_groups" {
+  description = "A list of security group ids for ingress"
+}
+
 variable "egress_rules" {
   description = "A list of rule names for egress"
   type        = list(string)
 }
 
+variable "egress_cidr_blocks" {
+  description = "A list of cidr blocks for egress"
+  type = list(string)
+}
+
+variable "egress_security_groups" {
+  description = "A list of security group ids for egress"
+}
 variable "sg_name" {
   type = string
 }
